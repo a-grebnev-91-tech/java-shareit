cb7077e86fdb82799e2ce12f774d55db090b843b
a-grebnev-91-tech <a.grebnev.91.tech@yandex.ru>
2022-09-08 16:57:23 +0300

    test: add for creating item request

--- src/main/java/ru/practicum/shareit/requests/controller/dto/RequestInputDto.java
+++ src/main/java/ru/practicum/shareit/requests/controller/dto/RequestInputDto.java
@@ -1,13 +1,15 @@
 package ru.practicum.shareit.requests.controller.dto;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 
 @Getter
 @Setter
+@EqualsAndHashCode
 public class RequestInputDto {
-    @NotNull
+    @NotBlank
     private String description;
 }
