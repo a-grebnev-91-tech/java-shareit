faf6fe3c5161eb81478087648c6c0851c0b3d1fe
a-grebnev-91-tech <a.grebnev.91.tech@yandex.ru>
2022-09-20 00:12:26 +0300

    refact: delete unused import

--- src/test/java/ru/practicum/shareit/booking/controller/BookingControllerParamObjectTest.java
+++ src/test/java/ru/practicum/shareit/booking/controller/BookingControllerParamObjectTest.java
@@ -14,7 +14,6 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
--- src/test/java/ru/practicum/shareit/item/controller/ItemControllerTest.java
+++ src/test/java/ru/practicum/shareit/item/controller/ItemControllerTest.java
@@ -10,7 +10,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.bind.MissingRequestHeaderException;
 import ru.practicum.shareit.exception.*;
 import ru.practicum.shareit.item.controller.dto.CommentInputDto;
 import ru.practicum.shareit.item.controller.dto.CommentOutputDto;
