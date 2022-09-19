40e7ba104d1e2e4acc3cbf560424f5e2e4e88948
a-grebnev-91-tech <a.grebnev.91.tech@yandex.ru>
2022-09-20 00:04:39 +0300

    refact: unused import

--- src/main/java/ru/practicum/shareit/requests/controller/dto/RequestOutputDto.java
+++ src/main/java/ru/practicum/shareit/requests/controller/dto/RequestOutputDto.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.requests.controller.dto;
 
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import ru.practicum.shareit.item.controller.dto.ItemOutputDto;
